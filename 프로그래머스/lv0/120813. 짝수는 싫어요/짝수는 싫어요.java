1ced71a326f4aa8d8e91daff26e08d4e7a163e66
tmxjvm5 <108376448+tmxjvm5@users.noreply.github.com>
2023-05-03 17:52:56 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.02 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] solution(int n) {
+        int[] answer = new int[(n+1)/2];
+        int temp = 1;
+
+        for(int i=0; i<answer.length;i++){
+            answer[i] = temp;
+            temp +=2;
+        }        
+        return answer;
+    }
+}
