8604d240a287787233b98344044a831f0525e29a
tmxjvm5 <108376448+tmxjvm5@users.noreply.github.com>
2023-05-08 21:47:51 +0900

    [Bronze III] Title: 알람 시계, Time: 236 ms, Memory: 18552 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int H = sc.nextInt();
+		int M = sc.nextInt();
+		if (M < 45) {
+			H--;
+			M = M + 15;
+			if (H < 0) {
+				H = 23;
+			}
+			System.out.println(H + " " + M);
+		} else {
+			System.out.println(H + " " + (M - 45));
+		}
+	}
+}
