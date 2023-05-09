dd34a16f09c5c1daee053d7553b1cb2a1d61c542
tmxjvm5 <108376448+tmxjvm5@users.noreply.github.com>
2023-05-09 16:38:06 +0900

    [Bronze V] Title: A+B - 7, Time: 156 ms, Memory: 16148 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int t = Integer.parseInt(br.readLine());
+		
+		for(int i = 1; i <= t; i++) {
+			String s = br.readLine();
+			int a = Integer.parseInt(s.split(" ")[0]);
+			int b = Integer.parseInt(s.split(" ")[1]);
+            bw.write("Case #"+i+":"+" "+(a+b)+"\n");
+		}
+			bw.flush();
+	}
+
+}
